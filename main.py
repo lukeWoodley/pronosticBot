d354432fa0141b5163e535c8d8385d06c69d7b58
Luke Woodley <luke.woodleyy@gmail.com>
2021-07-24 22:34:13 -0400

    new link

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ import time
 import pyautogui
 from datetime import datetime
 from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+
 PATH  = 'C:\Program Files (x86)\chromedriver.exe'
 
 def launchSelenium():
    driver = webdriver.Chrome(PATH)
-   driver.get('https://www.winamax.fr/paris-sportifs/match/28200766')#https://www.winamax.fr/paris-sportifs/match/28200766  #https://www.winamax.fr/paris-sportifs/match/27471666
+   driver.get('https://www.winamax.fr/paris-sportifs/match/27471658')#https://www.winamax.fr/paris-sportifs/match/28200766  #https://www.winamax.fr/paris-sportifs/match/27471666
    driver.maximize_window()
    while True:
-      driver.get('https://www.winamax.fr/paris-sportifs/match/27471666')
+      driver.get('https://www.winamax.fr/paris-sportifs/match/27471658')
       driver.maximize_window()
       time.sleep(getCurrentSeconds())
-      buttonScoreExact = driver.fignd_element_by_xpath(
+      buttonScoreExact = driver.find_element_by_xpath(
          '//*[@id="app"]/div/div[2]/div/div[2]/div/div[4]/div[1]/div/div[1]/div/div[5]/span/span[1]')
       buttonScoreExact.click()
       buttonScoreExact.location_once_scrolled_into_view
