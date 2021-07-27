68bfddf7938217fb87bd360e4f23d3c85b109c0f
Luke Woodley <luke.woodleyy@gmail.com>
2021-07-26 23:19:15 -0400

    looping tabs working

--- main.py
+++ main.py
@@ -4,50 +4,55 @@ import pyautogui
 from datetime import datetime
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
+import os
 
 PATH  = 'C:\Program Files (x86)\chromedriver.exe'
-xPath = '//*[@id="app"]/div/div[2]/div/div[2]/div/div[4]/div[1]/div/div[1]/div/div[5]/span/span[1]'
+xPath = '//*[@id="app"]/div/div[2]/div/div[2]/div/div[4]/div[1]/div/div[1]/div/div[3]/span/span[1]'
 arrayMatchAndLink = [
-                    ['Atlético Mineiro vs Bahia: Bresil Serie A','https://www.winamax.fr/paris-sportifs/match/27471658'],
-                    ['Huachipato vs Deportes La Serena: Chili premiere division','https://www.winamax.fr/paris-sportifs/match/27207554'],
-                    ['Changchun Yatai vs Dalian Professional FC: Chine super League','https://www.winamax.fr/paris-sportifs/match/28141760'],
-                    ['Bucaramanga vs Santa Fe: Colombie Primera A, Clausura','https://www.winamax.fr/paris-sportifs/match/28224720']
+                    ['Sao Paulo vs Palmeiras',      'https://www.winamax.fr/paris-sportifs/match/27471696'],
+                    ['Internacional vs Cuiaba MT',  'https://www.winamax.fr/paris-sportifs/match/27471694'],
+                    ['Bragantino SP vs Grêmio',     'https://www.winamax.fr/paris-sportifs/match/27471684'],
+                    ['Corinthians vs Flamengo',     'https://www.winamax.fr/paris-sportifs/match/27471690']
+
 ]
+
 
 
 def launchSelenium():
     driver = webdriver.Chrome(PATH)
     driver.maximize_window()
-    time.sleep(10)
-    #openAllTabs(driver)
+    time.sleep(2)
+    openAllTabs(driver)
 
-   # print("come here!")
-   #time.sleep(10)
-   #driver.navigate.refresh()
 
 def openAllTabs(driver):
-    i = 0
-    firstWindow = driver.window_handles[0]
-    firstWindow = "tab1"
+    while True:
+        firstIteration = True
+        i = 2
 
-    for row in arrayMatchAndLink:
-        if i == 0:
-            driver.get(row[1])
-        else:
-            nameOfTab = tab + str(i)
-            command = "window.open('about:blank','" + nameOfTab + "');"
-            driver.execute_script(command)
-            driver.switch_to.window(nameOfTab)
-            driver.get(row[1])
+        for row in arrayMatchAndLink:
+            if firstIteration:
+                driver.get(row[1])
+                firstIteration = False
+            else:
+                nameOfTab = "tab" + str(i)
+                command = "window.open('about:blank','" + nameOfTab + "');"
+                driver.execute_script(command)
+                driver.switch_to.window(nameOfTab)
+                driver.get(row[1])
+            i = i + 1
+
+        driver.switch_to.window(driver.window_handles[0])
+        time.sleep(5)
+        indexTab = 0
+        for row in arrayMatchAndLink:
+            driver.switch_to.window(driver.window_handles[indexTab])
+            driver.refresh()
             time.sleep(3)
             driver = goToScoreExact(driver)
             takeScreenshot(row[0])
-
-    driver.get('https://www.winamax.fr/paris-sportifs/match/27471658')
-    driver.maximize_window()
-    driver.execute_script("window.open('about:blank','secondtab');")
-    driver.switch_to.window("secondtab")
-    driver.get('https://www.winamax.fr/paris-sportifs/match/27471658')
+            indexTab = indexTab + 1
+        time.sleep(getCurrentSeconds())
 
 
 def goToScoreExact(driver):
@@ -57,41 +62,28 @@ def goToScoreExact(driver):
     return driver
 
 
-
-
-   # driver = webdriver.Chrome(PATH)
-   # driver.get('https://www.winamax.fr/paris-sportifs/match/28200766')#https://www.winamax.fr/paris-sportifs/match/28200766  #https://www.winamax.fr/paris-sportifs/match/27471666
-   # driver.maximize_window()
-   # while True:
-   #    driver.get('https://www.winamax.fr/paris-sportifs/match/27471666')
-   #    driver.maximize_window()
-   #    time.sleep(getCurrentSeconds())
-   #    buttonScoreExact = driver.fignd_element_by_xpath(
-   #       '//*[@id="app"]/div/div[2]/div/div[2]/div/div[4]/div[1]/div/div[1]/div/div[5]/span/span[1]')
-   #    buttonScoreExact.click()
-   #    buttonScoreExact.location_once_scrolled_into_view
-   #    time.sleep(2)
-   #    takeScreenshot(driver)
-
-
-
-
 def getCurrentSeconds():
    now = datetime.now()
    current_time = int(now.strftime("%S"))
    return 60-current_time
 
 
-
-
-
 def takeScreenshot(folderName):
    now = datetime.now()
    current_time = now.strftime("%d_%m_%Y %H_%M_%S")
    myscreen = pyautogui.screenshot()
-   pathScreenShot = folderName + "/" + current_time+".png"
+   pathScreenShot =  folderName + "/" + current_time+".png"
    print(pathScreenShot)
+   os.chdir("matches_pictures/")
+   createDirectory(folderName)
    myscreen.save(pathScreenShot)
+   os.chdir("..")
+
+def createDirectory(folderName):
+    try:
+        os.mkdir(folderName)
+    except OSError as error:
+        pass
 
 
 if __name__ == '__main__':
