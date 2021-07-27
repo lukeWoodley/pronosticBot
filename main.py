68a503ba314b35effdff3b8e2fefc0a5dfee3d9a
Luke Woodley <luke.woodleyy@gmail.com>
2021-07-27 01:07:09 -0400

    added current matches

--- main.py
+++ main.py
@@ -7,14 +7,24 @@ from selenium import webdriver
 import os
 
 PATH  = 'C:\Program Files (x86)\chromedriver.exe'
+
 xPath = '//*[@id="app"]/div/div[2]/div/div[2]/div/div[4]/div[1]/div/div[1]/div/div[3]/span/span[1]'
+xPath2 = '//*[@id="app"]/div/div[2]/div/div[2]/div/div[4]/div[1]/div/div[1]/div/div[4]/span/span[1]'
+xPath3 = '//*[@id="app"]/div/div[2]/div/div[2]/div/div[4]/div[1]/div/div[1]/div/div[5]/span/span[1]'
+
+
+
 arrayMatchAndLink = [
-                    ['Sao Paulo vs Palmeiras',      'https://www.winamax.fr/paris-sportifs/match/27471696'],
-                    ['Internacional vs Cuiaba MT',  'https://www.winamax.fr/paris-sportifs/match/27471694'],
-                    ['Bragantino SP vs Grêmio',     'https://www.winamax.fr/paris-sportifs/match/27471684'],
-                    ['Corinthians vs Flamengo',     'https://www.winamax.fr/paris-sportifs/match/27471690']
+                    ['Audax Italiano vs Everton',                    'https://www.winamax.fr/paris-sportifs/match/27207560', xPath2],
+                    ['Vikingur Gota vs Runavik',                     'https://www.winamax.fr/paris-sportifs/match/25853620', xPath2],
+                    ['07 Vestur Sorvagur vs Streymur',               'https://www.winamax.fr/paris-sportifs/match/25853616', xPath],
+                    ['La Equidad vs Pasto',                          'https://www.winamax.fr/paris-sportifs/match/28224728', xPath3],
+                    ['Huracan vs Colon De Santa Fe',                 'https://www.winamax.fr/paris-sportifs/match/27959198', xPath2],
+                    ['Aldosivi vs Racing Club',                      'https://www.winamax.fr/paris-sportifs/match/27959186', xPath2]
 
 ]
+
+
 
 
 
@@ -26,22 +36,22 @@ def launchSelenium():
 
 
 def openAllTabs(driver):
+    firstIteration = True
+    i = 2
+
+    for row in arrayMatchAndLink:
+        if firstIteration:
+            driver.get(row[1])
+            firstIteration = False
+        else:
+            nameOfTab = "tab" + str(i)
+            command = "window.open('about:blank','" + nameOfTab + "');"
+            driver.execute_script(command)
+            driver.switch_to.window(nameOfTab)
+            driver.get(row[1])
+        i = i + 1
+
     while True:
-        firstIteration = True
-        i = 2
-
-        for row in arrayMatchAndLink:
-            if firstIteration:
-                driver.get(row[1])
-                firstIteration = False
-            else:
-                nameOfTab = "tab" + str(i)
-                command = "window.open('about:blank','" + nameOfTab + "');"
-                driver.execute_script(command)
-                driver.switch_to.window(nameOfTab)
-                driver.get(row[1])
-            i = i + 1
-
         driver.switch_to.window(driver.window_handles[0])
         time.sleep(5)
         indexTab = 0
@@ -49,14 +59,15 @@ def openAllTabs(driver):
             driver.switch_to.window(driver.window_handles[indexTab])
             driver.refresh()
             time.sleep(3)
-            driver = goToScoreExact(driver)
+            driver = goToScoreExact(driver, row)
+            time.sleep(0.2)
             takeScreenshot(row[0])
             indexTab = indexTab + 1
         time.sleep(getCurrentSeconds())
 
 
-def goToScoreExact(driver):
-    buttonScoreExact = driver.find_element_by_xpath(xPath)
+def goToScoreExact(driver, row):
+    buttonScoreExact = driver.find_element_by_xpath(row[2])
     buttonScoreExact.click()
     buttonScoreExact.location_once_scrolled_into_view
     return driver
@@ -65,7 +76,7 @@ def goToScoreExact(driver):
 def getCurrentSeconds():
    now = datetime.now()
    current_time = int(now.strftime("%S"))
-   return 60-current_time
+   return 120-current_time
 
 
 def takeScreenshot(folderName):
